e03ff7bc245afc75b62ecffe9667ca2df7e1506e
Yigir-del <altuntasy83@gmail.com>
2025-03-13 15:28:08 +0300

    Add files via upload

---
+++ ubys_bot/mesage.py
@@ -0,0 +1,11 @@
+import requests
+
+def data(TOKEN,CHAT_ID,mesage):
+    TOKEN = TOKEN
+    CHAT_ID = CHAT_ID
+    MESAJ = mesage
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    payload = {"chat_id": CHAT_ID, "text": MESAJ}
+    response = requests.post(url, json=payload)
+
+
---
+++ ubys_bot/users.py
@@ -0,0 +1,13 @@
+user_list =[
+    {
+    "name":"user_name",
+    "password":"user_password",
+    "sapid":"user_sapid"
+    },
+    {
+    "name":"user_name",
+    "password":"user_password",
+    "sapid":"user_sapid"
+    }
+]
+
